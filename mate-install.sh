0dadf47556649e81ab5df743cadc8728cecfbff6
CaptainZalo <captzalo@gmail.com>
2016-09-16 10:56:35 +0200

    Update mate-install.sh

--- mate-install.sh
+++ mate-install.sh
@@ -15,38 +15,38 @@ fi
 sudo apt-get -y update
 # sudo apt-get install mate-desktop-environment-extras
 # Install packages without dialogs
-sudo apt-get -y install --no-install-recommends mate-desktop-environment-extras mate-archive-keyring caja-gksu caja-sendto mate-indicator-applet mate-media-gstreamer mate-icon-theme-faenza
-# Remove substituted packages
-sudo apt-get -y remove gcalctool gnome-screenshot gedit file-roller eog gnome-system-monitor gnome-system-log baobab gnome-terminal gnome-applets gnome-media gnome-power-manager gnome-screensaver
+sudo apt-get -y install mate-desktop-environment-extras caja-gksu caja-sendto mate-indicator-applet mate-icon-theme-faenza
+# Remove substituted packages (commented out)
+# sudo apt-get -y remove gcalctool gnome-screenshot gedit file-roller eog gnome-system-monitor gnome-system-log baobab gnome-terminal gnome-applets gnome-media gnome-power-manager gnome-screensaver
 # Set default desktop for new users (fails)
 # sudo /usr/lib/lightdm/lightdm-set-defaults -s mate
-# Change MIME types
+# Change MIME types and terminal emulator
 # Folders:
-xdg-mime default caja-folder-handler.desktop inode/directory
+sudo xdg-mime default caja-folder-handler.desktop inode/directory
 # SSH sites:
-xdg-mime default caja-folder-handler.desktop x-scheme-handler/ssh
+sudo xdg-mime default caja-folder-handler.desktop x-scheme-handler/ssh
 # FTP sites:
-xdg-mime default caja-folder-handler.desktop x-scheme-handler/ftp
+sudo xdg-mime default caja-folder-handler.desktop x-scheme-handler/ftp
 # Images:
-xdg-mime default eom.desktop image/bmp
-xdg-mime default eom.desktop image/gif
-xdg-mime default eom.desktop image/jpeg
-xdg-mime default eom.desktop image/x-pcx
-xdg-mime default eom.desktop image/png
-xdg-mime default eom.desktop image/tiff
+sudo xdg-mime default eom.desktop image/bmp
+sudo xdg-mime default eom.desktop image/gif
+sudo xdg-mime default eom.desktop image/jpeg
+sudo xdg-mime default eom.desktop image/x-pcx
+sudo xdg-mime default eom.desktop image/png
+sudo xdg-mime default eom.desktop image/tiff
 # Plain text:
-xdg-mime default pluma.desktop text/plain
-xdg-mime default pluma.desktop text/x-log
-xdg-mime default pluma.desktop application/x-perl
-xdg-mime default pluma.desktop application/javascript
-xdg-mime default pluma.desktop application/rdf+xml
-# If you want for Wine plain text files:
-xdg-mime default pluma.desktop application/x-wine-extension-ini
-xdg-mime default pluma.desktop application/x-wine-extension-vbs
-xdg-mime default pluma.desktop text/x-csrc
-xdg-mime default pluma.desktop application/x-wine-extension-inf
+sudo xdg-mime default pluma.desktop text/plain
+sudo xdg-mime default pluma.desktop text/x-log
+sudo xdg-mime default pluma.desktop application/x-perl
+sudo xdg-mime default pluma.desktop application/javascript
+sudo xdg-mime default pluma.desktop application/rdf+xml
+# Wine plain text files:
+sudo xdg-mime default pluma.desktop application/x-wine-extension-ini
+sudo xdg-mime default pluma.desktop application/x-wine-extension-vbs
+sudo xdg-mime default pluma.desktop text/x-csrc
+sudo xdg-mime default pluma.desktop application/x-wine-extension-inf
 # PDF:
-xdg-mime default atril.desktop application/pdf
-# Change default terminal emulator appli
+sudo xdg-mime default atril.desktop application/pdf
+# Default terminal emulator app:
 sudo update-alternatives --install "$(which x-terminal-emulator)" x-terminal-emulator "$(which mate-terminal)" 30
 sudo update-alternatives --set x-terminal-emulator "$(which mate-terminal)"
